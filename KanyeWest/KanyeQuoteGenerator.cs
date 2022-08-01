3a6f4364773e2ca086e0bf2b988c57a77d967081
Damion Russell <domino.russell@gmail.com>
2022-08-01 10:28:05 -0400

    intial commit

---
+++ KanyeWest/KanyeQuoteGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeWest
+{
+    public class KanyeQuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+            var client = new HttpClient();
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine(kanyeQuote);
+        }
+    }
+}
---
+++ KanyeWest/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                KanyeQuoteGenerator.KanyeQuote();
+                RonQuoteGenerator.RonQuote();
+            }
+        }
+    }
+}
